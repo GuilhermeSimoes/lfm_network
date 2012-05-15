008d2e68dd1305d3a7e0e2a88e202eb20d6dd414
GuilhermeSimoes <GuilhermeMicaelo@gmail.com>
2012-05-15 17:02:40 +0100

    Now caching assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,14 +8,17 @@ LfmNetwork::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # Enable Rails's static asset server (Apache or nginx will not need this)
+  config.serve_static_assets = true
+
+  # Set expire header of 30 days for static files
+  config.static_cache_control = "public, max-age=2592000"
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -50,10 +53,6 @@ LfmNetwork::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
