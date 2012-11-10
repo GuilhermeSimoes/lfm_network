d3db42947157b7628600a61f4730076ca3df569d
Guilherme Simoes <guilherme.rdems@gmail.com>
2012-11-10 22:31:55 +0000

    Removed useless to_str methods.

--- app/controllers/fetch_controller.rb
+++ app/controllers/fetch_controller.rb
@@ -1,7 +1,7 @@
 class FetchController < ApplicationController
   def index
   end
-  
+
   def user
     respond_to do |format|
       format.html {
@@ -10,10 +10,10 @@ class FetchController < ApplicationController
       }
       format.xml {
         username = params[:user].gsub(' ','+')
-        
+
         get_info_url = "http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=#{username}&api_key=#{API_KEY}"
         get_friends_url = "http://ws.audioscrobbler.com/2.0/?method=user.getfriends&user=#{username}&api_key=#{API_KEY}&limit=20"
-        
+
         begin
           info_xml = Nokogiri::XML(open(get_info_url))
           friends_xml = Nokogiri::XML(open(get_friends_url))
@@ -22,27 +22,27 @@ class FetchController < ApplicationController
             xml.lfm {
               xml.user {
                 user = info_xml.at_xpath('//user')
-                xml << user.at_xpath('name').to_xml.to_str
-                xml << user.at_xpath('realname').to_xml.to_str
-                xml << user.at_xpath('url').to_xml.to_str
-                xml << user.at_xpath('playcount').to_xml.to_str
-                xml << user.xpath('image').to_xml.to_str
-                
+                xml << user.at_xpath('name').to_xml
+                xml << user.at_xpath('realname').to_xml
+                xml << user.at_xpath('url').to_xml
+                xml << user.at_xpath('playcount').to_xml
+                xml << user.xpath('image').to_xml
+
                 xml.score "1"
               }
-              
+
               xml.friends {
                 friends_xml.xpath('lfm/friends/user').each do |user|
                   xml.user {
-                    xml << user.at_xpath('name').to_xml.to_str
-                    xml << user.at_xpath('realname').to_xml.to_str
-                    xml << user.at_xpath('url').to_xml.to_str
-                    xml << user.at_xpath('playcount').to_xml.to_str
-                    xml << user.xpath('image').to_xml.to_str
-                    
+                    xml << user.at_xpath('name').to_xml
+                    xml << user.at_xpath('realname').to_xml
+                    xml << user.at_xpath('url').to_xml
+                    xml << user.at_xpath('playcount').to_xml
+                    xml << user.xpath('image').to_xml
+
                     tasteometer_url = "http://ws.audioscrobbler.com/2.0/?method=tasteometer.compare&type1=user&type2=user&value1=#{username}&value2=#{user.at_xpath('name').text}&api_key=#{API_KEY}"
                     taste_xml = Nokogiri::XML(open(tasteometer_url))
-                    xml << taste_xml.at_xpath('//score').to_xml.to_str
+                    xml << taste_xml.at_xpath('//score').to_xml
                   }
                 end
               }
@@ -51,7 +51,6 @@ class FetchController < ApplicationController
         rescue Exception => e
           # puts e.message
           # puts e.backtrace.join("\n")
-        
           if e.message == '400 Bad Request'
             render xml: { error: 'User not found.' }, status: 400
           else
